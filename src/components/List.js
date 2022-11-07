8b00ec1e329fe53906e8a33e7e415a72a99d3e86
Renes Dwi Riwanto <renesriwanto@gmail.com>
2022-11-07 20:48:21 +0700

    init listitem component

--- src/components/List.js
+++ src/components/List.js
@@ -1,7 +1,11 @@
+import ListItem from "./ListItem";
+
 function List() {
   return (
     <div>
       <h1>list</h1>
+      <ListItem />
+      <ListItem />
     </div>
   );
 }
---
+++ src/components/ListItem.js
@@ -0,0 +1,11 @@
+function ListItem() {
+  return (
+    <div>
+      <h1>LIST ITEM</h1>
+      <h1>LIST ITEM</h1>
+      <h1>LIST ITEM</h1>
+    </div>
+  );
+}
+
+export default ListItem;
